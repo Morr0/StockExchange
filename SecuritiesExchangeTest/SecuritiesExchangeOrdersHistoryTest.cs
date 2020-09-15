72d46e2c9a54967323f925a411da7b1db4d49e75
Rami Hikmat <rami@ramihikmat.net>
2020-09-15 14:14:33 +1000

    Successful order history

---
+++ SecuritiesExchangeTest/SecuritiesExchangeOrdersHistoryTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using StockExchangeWeb.Models.Orders;
+using StockExchangeWeb.Services;
+using StockExchangeWeb.Services.HistoryService;
+using StockExchangeWeb.Services.TradedEntitiesService;
+using Xunit;
+
+namespace SecuritiesExchangeTest
+{
+    public class SecuritiesExchangeOrdersHistoryTest
+    {
+        private OrdersHistoryRepository _ordersHistory = new OrdersHistoryRepository();
+        private static ISecuritiesProvider _securitiesProvider = new SecuritiesProvider();
+
+        [Fact]
+        public async Task PerformBasicTradeWhileCheckingHistoryIsCorrect()
+        {
+            // Arrange
+            IStockExchange stockExchange = new InMemoryStockExchangeRepository(_securitiesProvider, _ordersHistory);
+            string ticker = "A";
+            uint amount = 500;
+            decimal askPrice = 13.0m;
+            string buyOrderId = Guid.NewGuid().ToString();
+            string sellOrderId = Guid.NewGuid().ToString();
+            Order sellOrder = new Order
+            {
+                Ticker = ticker,
+                Amount = amount,
+                AskPrice = askPrice,
+                BuyOrder = true,
+                Id = sellOrderId
+            };
+            Order buyOrder = new Order
+            {
+                Ticker = ticker,
+                Amount = amount,
+                AskPrice = askPrice,
+                BuyOrder = false,
+                Id = buyOrderId
+            };
+            
+            // Act I
+            await stockExchange.PlaceOrder(sellOrder);
+            
+            // Assert I
+            Assert.True(_ordersHistory._archivedOrders.ContainsKey(sellOrderId));
+            Assert.Equal(OrderStatus.IN_MARKET, _ordersHistory._archivedOrders[sellOrderId].OrderStatus);
+            
+            // Act II
+            await stockExchange.PlaceOrder(buyOrder);
+            
+            // Assert II
+            Assert.True(_ordersHistory._archivedOrders.ContainsKey(buyOrderId));
+            Assert.Equal(OrderStatus.EXECUTED, _ordersHistory._archivedOrders[sellOrderId].OrderStatus);
+            Assert.Equal(OrderStatus.EXECUTED, _ordersHistory._archivedOrders[buyOrderId].OrderStatus);
+
+        }
+    }
+}
--- StockExchangeWeb/Services/HistoryService/OrdersHistoryRepository.cs
+++ StockExchangeWeb/Services/HistoryService/OrdersHistoryRepository.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using StockExchangeWeb.Models.Orders;
 
+[assembly: InternalsVisibleTo("SecuritiesExchangeTest")]
 namespace StockExchangeWeb.Services.HistoryService
 {
     public class OrdersHistoryRepository : IOrdersHistory
     {
-        private Dictionary<string, Order> _archivedOrders = new Dictionary<string, Order>();
+        internal Dictionary<string, Order> _archivedOrders = new Dictionary<string, Order>();
         
         public async Task ArchiveOrder(Dictionary<string, Order> orders)
         {
